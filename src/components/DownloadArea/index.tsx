1ba1e250a6d5536b562e99b65951c7c077567603
Danilove <dansousa0(@gmail.com>
2022-05-01 14:10:16 -0300

    feat: using hook to download file

--- src/components/DownloadArea/index.tsx
+++ src/components/DownloadArea/index.tsx
@@ -1,15 +1,21 @@
+import useDownloader from 'react-use-downloader'
 import * as C from './styles'
 
 const DownloadArea = () => {
+    const { size, elapsed, percentage, download, cancel, error, isInProgress } = useDownloader();
 
-  return (
-    <C.Container    >
-      <C.DownloadIcon
-      />
-      <C.DownloadText>Faça o download da planilha exemplo</C.DownloadText>
-      <C.DownloadSubText>(Preencha os dados de acordo com a marcação indicada)</C.DownloadSubText>
-    </C.Container>
-  )
+    const fileUrl = "/vercel.svg";
+    const fileName = "vercel.svg";
+
+    return (
+        <C.Container >
+            <C.DownloadIcon
+                onClick={() => download(fileUrl, fileName)}
+            />
+            <C.DownloadText>Faça o download da planilha exemplo</C.DownloadText>
+            <C.DownloadSubText>(Preencha os dados de acordo com a marcação indicada)</C.DownloadSubText>
+        </C.Container>
+    )
 }
 
 export default DownloadArea
