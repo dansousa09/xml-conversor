d5fdf8874cf25d1bd04daf6271a82244dd203af9
Danilove <dansousa0(@gmail.com>
2022-05-01 20:07:56 -0300

    feat: create input and function to convert csv file to json

--- src/components/UploadArea/index.tsx
+++ src/components/UploadArea/index.tsx
@@ -1,9 +1,37 @@
+import Papa from "papaparse";
+import { ChangeEvent } from "react";
+
 import * as C from './styles'
 
 const UploadArea = () => {
-  return (
-    <C.Container>UploadArea</C.Container>
-  )
+
+    const handleFileUpload = (e: ChangeEvent<HTMLInputElement>) => {
+        const files = e.target.files;
+        console.log(files);
+
+        if (files) {
+            console.log(files[0]);
+            Papa.parse(files[0], {
+                complete: function (results) {
+                    console.log("Finished:", results.data);
+                },
+                error: function (err) {
+                    console.log("Error", err);
+                }
+            }
+            )
+        } else {
+            console.log("No file selected");
+        }
+    }
+
+    return (
+        <C.Container>
+            <input id="upload" type="file"
+                onChange={(e) => handleFileUpload(e)}
+            />
+        </C.Container>
+    )
 }
 
 export default UploadArea
--- src/utils/csvExample.js
+++
@@ -1,6 +0,0 @@
-let csvData = `product,pakageing,qty
-   mno,100ML,200
-   pqr,400ML,300
-   abc,150ML,100`;
-
-export default csvData;
