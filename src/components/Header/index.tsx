dca7afad1a93f34d22a5206191d07c64f2f01812
Danilo <danilo.elias@livsaude.com.br>
2022-05-03 16:27:26 -0300

    feat: adds a button to download the ANS guide

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,32 @@
+import useDownloader from 'react-use-downloader';
+import { useLoading } from '../../context/loading'
 import * as C from './styles'
 
 const Header = () => {
+  const { setLoading } = useLoading();
+  const { download, isInProgress } = useDownloader()
+
+  const handleDownloadGuide = () => {
+    isInProgress && setLoading(true);
+
+    download("/Manual_orientacoes_RPS_WEB_XML.pdf", "Manual_orientacoes_RPS_WEB_XML.pdf");
+
+    !isInProgress && setLoading(false);
+  }
+
   return (
     <C.Container>
-      <h1>XML-Conversor</h1>
+      <C.Title variant='h1' >XML-Conversor</C.Title>
+      <C.DownloadGuide onClick={handleDownloadGuide} >
+        <label htmlFor="download-guide-btn" >Download Manual - ANS</label>
+        <C.IconButton name="download-guide-btn" id="download-guide-btn" >
+          <C.DownloadIcon />
+        </C.IconButton>
+      </C.DownloadGuide>
     </C.Container>
   )
 }
 
 export default Header
+
+
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,11 +1,55 @@
 import styled from "styled-components";
+import { IconButton as MuiIconButton, Typography } from "@mui/material";
+import { AiOutlineDownload } from "react-icons/ai";
 
 export const Container = styled.header`
     width: 100vw;
-    height: 10vh; 
+    height: 10vh;
 
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+
+    background: linear-gradient(
+        to right,
+        rgb(32, 106, 131) 0%,
+        rgb(23, 48, 70) 100%
+    );
+`;
+
+export const Title = styled(Typography)`
+    font-size: 2rem;
+    font-weight: bold;
+    color: ${(props) => props.theme.colors.background};
+`;
+
+export const DownloadGuide = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
 
+    padding: 0 1rem;
+    border-radius: 0.5rem;
+
+    background-color: ${(props) => props.theme.colors.background};
+
+    gap: 0.5rem;
+
+    transform: scale(0.95);
+    transition: all 0.3s ease;
+
+    cursor: pointer;
+    > label {
+        cursor: pointer;
+    }
+
+    &:hover {
+        /* opacity: 0.9; */
+        transform: scale(1);
+    }
 `;
+
+export const IconButton = styled(MuiIconButton)``;
+
+export const DownloadIcon = styled(AiOutlineDownload)``;
